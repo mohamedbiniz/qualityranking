0e3a3a2498b6e722cd34f8882dc6bda3e00b3401
fabriciorsf <fabriciorsf@baa973d8-b457-11dd-81ae-51688d75609f>
2008-11-20 21:53:45 +0000

    arquivos de configuração e do matlab

---
+++ QualityRanking/conf/FoxSetMATLAB.m
@@ -0,0 +1,4 @@
+addpath 'C:\Users\Heraldo\Documents\PESC\P2 - Busca e Recuperação de Informação\Crawler\QualityRanking\bin'
+javaaddpath 'C:\Users\Heraldo\Documents\PESC\P2 - Busca e Recuperação de Informação\Crawler\QualityRanking\bin'
+ms = br.ufrj.cos.bri.matlab.server.MatServer;
+ms.body();
