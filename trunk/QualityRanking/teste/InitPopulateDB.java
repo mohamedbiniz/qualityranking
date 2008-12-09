c6c011568231fd2b21d943ff94296acfe0c9c2dd
fabriciorsf <fabriciorsf@baa973d8-b457-11dd-81ae-51688d75609f>
2008-12-09 02:33:30 +0000

    init db

--- trunk/QualityRanking/teste/InitPopulateDB.java
+++ trunk/QualityRanking/teste/InitPopulateDB.java
@@ -8,7 +8,7 @@ import br.ufrj.cos.db.PopulateDB;
  * @author Fabricio
  * 
  */
-public class TestePopulateDB {
+public class InitPopulateDB {
 	public static void main(String[] args) {
 		try {
 			PopulateDB populate = (new PopulateDB());
