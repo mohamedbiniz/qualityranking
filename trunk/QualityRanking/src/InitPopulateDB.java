78b08a3ffbefc68eb768913d4e3cb624ff401f25
fabriciorsf <fabriciorsf@baa973d8-b457-11dd-81ae-51688d75609f>
2008-12-15 04:12:12 +0000

    comentários úteis

--- trunk/QualityRanking/src/InitPopulateDB.java
+++ trunk/QualityRanking/src/InitPopulateDB.java
@@ -12,11 +12,25 @@ public class InitPopulateDB {
 	public static void main(String[] args) {
 		try {
 			PopulateDB.limparDB();
+
+			// Descomentar abaixo para inicializar o BD a fim de permitir
+			// sistema entrar em produção de modo correto
 			PopulateDB.initFoxSet();
+
+			// Descomentar abaixo para testar o pOfN (contexto economia)
 			// PopulateDB.popularSearchPofN(5);
+
+			// Descomentar abaixo para testar o quality fuzzy (contexto
+			// economia)
 			// PopulateDB.popularSearchQF(5);
+
+			// Descomentar abaixo para testar o crawler com contexto de economia
 			// PopulateDB.popularTradicionalEconomia();
+
+			// Descomentar abaixo para testar o crawler com contexto de banco de
+			// dados relacinal
 			// PopulateDB.popularTradicionalBDR();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
