0fbd3c1b81206ca2168ead0db9120f4665d2db21
fabriciorsf <fabriciorsf@baa973d8-b457-11dd-81ae-51688d75609f>
2008-12-17 11:15:38 +0000

    atualizacao no esquemas do crawler

--- trunk/QualityRanking/db/esquemaTabelas.sql
+++ trunk/QualityRanking/db/esquemaTabelas.sql
@@ -1,23 +1,5 @@
-DROP TABLE IF EXISTS `foxset`.`outputlink_CRAWLER`;
-CREATE TABLE  `foxset`.`outputlink_CRAWLER` (
-  `ID` bigint(20) NOT NULL,
-  `DATE_CREATE` datetime default NULL,
-  `DOMAIN` varchar(255) default NULL,
-  `IdDataSet` bigint(20) default 0,
-  `IdPagina` bigint(20)  default 0,
-  `ID_MD5` bigint(20)  default 0,
-  `LAST_MODIFIED` datetime default NULL,
-  `NEXT_FETCH` datetime default NULL,
-  `ORDEM_DOWNLOAD_PAI` bigint(20) default 0,
-  `SCORE` double default 0,
-  `URL` longtext,
-  `VISITED` bit(1) ,
-  `SEED` bit(1),
-  PRIMARY KEY  (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `foxset`.`metadata_CRAWLER`;
-CREATE TABLE  `foxset`.`metadata_CRAWLER` (
+DROP TABLE IF EXISTS `foxset`.`metadata_crawler`;
+CREATE TABLE  `foxset`.`metadata_crawler` (
   `ID` bigint(20) NOT NULL,
   `IdPagina` bigint(20) default NULL,
   `NAME` varchar(255) default NULL,
@@ -27,8 +9,26 @@ CREATE TABLE  `foxset`.`metadata_CRAWLER` (
   PRIMARY KEY  (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `foxset`.`page_CRAWLER`;
-CREATE TABLE  `foxset`.`page_CRAWLER` (
+DROP TABLE IF EXISTS `foxset`.`outputlink_crawler`;
+CREATE TABLE  `foxset`.`outputlink_crawler` (
+  `ID` bigint(20) NOT NULL,
+  `DATE_CREATE` datetime default NULL,
+  `DOMAIN` varchar(255) default NULL,
+  `IdDataSet` bigint(20) default '0',
+  `IdPagina` bigint(20) default '0',
+  `ID_MD5` bigint(20) default '0',
+  `LAST_MODIFIED` datetime default NULL,
+  `NEXT_FETCH` datetime default NULL,
+  `ORDEM_DOWNLOAD_PAI` bigint(20) default '0',
+  `SCORE` double default '0',
+  `URL` longtext,
+  `VISITED` bit(1) default NULL,
+  `SEED` bit(1) default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `foxset`.`page_crawler`;
+CREATE TABLE  `foxset`.`page_crawler` (
   `ID` bigint(20) NOT NULL,
   `IdDataSet` bigint(20) default NULL,
   `page_id` bigint(20) default NULL,
@@ -36,7 +36,7 @@ CREATE TABLE  `foxset`.`page_CRAWLER` (
   `SCORE` double default NULL,
   `VERSION` bigint(20) default NULL,
   `URL` varchar(255) default NULL,
-  `CONTENT` LONGTEXT default NULL,
+  `CONTENT` longtext,
   `PATH_PAGE` varchar(255) default NULL,
   `DATE_CREATE` datetime default NULL,
   `LAST_MODIFIED` datetime default NULL,
