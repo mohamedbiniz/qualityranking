1198199c1e737f1326b702c151bfc4f3bec80f2d
fabriciorsf <fabriciorsf@baa973d8-b457-11dd-81ae-51688d75609f>
2009-02-06 03:09:13 +0000

    para vizualizar o jung

--- trunk/QualityRanking/teste/jung/teste/GraphInstanceTest.java
+++ trunk/QualityRanking/teste/jung/teste/GraphInstanceTest.java
@@ -2,89 +2,57 @@ package jung.teste;
 
 import jung.GraphInstance;
 import junit.framework.TestCase;
-
-
-
 import edu.uci.ics.jung.algorithms.importance.HITS;
-
-import edu.uci.ics.jung.algorithms.importance.PageRank;
-
-import edu.uci.ics.jung.graph.*;
-
-import edu.uci.ics.jung.graph.impl.SparseGraph;
-
-import edu.uci.ics.jung.graph.impl.DirectedSparseGraph;
-
-import junit.framework.TestCase;
-
-import edu.uci.ics.jung.algorithms.transformation.DirectionTransformer; 
-
-
+import edu.uci.ics.jung.graph.Graph;
 
 public class GraphInstanceTest extends TestCase {
 
-	
-
-GraphInstance graphInstance = new GraphInstance();
-
-	
+	static GraphInstance graphInstance = new GraphInstance();
 
 	/*
-
+	 * 
 	 * Test method for 'br.ufrj.HITS'
-
+	 * 
 	 */
 
-	public void testload() {
+	public static void main(String[] args) {
+		testload();
+	}
 
-		
+	public static void testload() {
 
 		/**
-
+		 * 
 		 * Chama o metodo que cria um grafo vertice a vertice
-
+		 * 
 		 */
 
 		/*
+		 * 
+		 * for(int i = 0; i < 10 ; i++) {
+		 * 
+		 * graphInstance.createGraph(new Object[]{"Term"+i}); }
+		 * 
+		 * graphInstance.createGraph(new Object[]{"Term"+3});
+		 * 
+		 * graphInstance.createGraph(new Object[]{"Term"+8});
+		 * 
+		 * graphInstance.createGraph(new Object[]{"Term"+6});
+		 * 
+		 * graphInstance.createGraph(new Object[]{"Term"+7});
+		 * 
+		 */
 
-		for(int i = 0; i < 10 ; i++)
-
-		{
-
-			graphInstance.createGraph(new Object[]{"Term"+i});
-
-			
-
-		}
-
-		graphInstance.createGraph(new Object[]{"Term"+3});
-
-		graphInstance.createGraph(new Object[]{"Term"+8});
-
-		graphInstance.createGraph(new Object[]{"Term"+6});
-
-		graphInstance.createGraph(new Object[]{"Term"+7});
-
-		*/
-
-		
-
-		Graph graph = graphInstance.load("/Users/mayworm/Documents/pessoais/mestrado/tese/workspace/Jung/teste/jung/teste/graphTest2.txt");
-
-		
+		Graph graph = graphInstance.load("pajek578.txt");
 
 		graphInstance.displayGraph(graph);
 
-		
-
-		//graphInstance.save(graph,"C:/eclipse/workspace/jung/graphTestOut.txt");
-
-		
+		// graphInstance.save(graph,"C:/eclipse/workspace/jung/graphTestOut.txt");
 
 		/*
-
+		 * 
 		 * Calcula Authorities
-
+		 * 
 		 */
 
 		System.out.println(" Ranking usando Authorities ");
@@ -96,9 +64,9 @@ GraphInstance graphInstance = new GraphInstance();
 		rankerAuthorities.printRankings(true, true);
 
 		/*
-
+		 * 
 		 * Calcula Hubs
-
+		 * 
 		 */
 
 		System.out.println(" Ranking usando Hubs ");
@@ -110,27 +78,24 @@ GraphInstance graphInstance = new GraphInstance();
 		rankerHubs.printRankings(true, true);
 
 		/*
-
+		 * 
 		 * Calcula PageRank
-
+		 * 
 		 */
 
-		System.out.println(" Ranking usando Pagerank ");
-
-		DirectedGraph directedGraph = new DirectedSparseGraph();
-
-		directedGraph = DirectionTransformer.toDirected(graph);
-
-		//directedGraph= (DirectedSparseGraph)graph;
-
-		PageRank rankerPageRank = new PageRank(directedGraph, 0.15);
-
-		rankerPageRank.evaluate();
-
-		rankerPageRank.printRankings(true, true);
-
-		
-
+		// System.out.println(" Ranking usando Pagerank ");
+		//
+		// DirectedGraph directedGraph = new DirectedSparseGraph();
+		//
+		// directedGraph = DirectionTransformer.toDirected(graph);
+		//
+		// // directedGraph= (DirectedSparseGraph)graph;
+		//
+		// PageRank rankerPageRank = new PageRank(directedGraph, 0.15);
+		//
+		// rankerPageRank.evaluate();
+		//
+		// rankerPageRank.printRankings(true, true);
 		try {
 
 			Thread.sleep(200000);
@@ -141,11 +106,8 @@ GraphInstance graphInstance = new GraphInstance();
 
 			e.printStackTrace();
 
-		}				
+		}
 
-	}	
-
-
-
+	}
 
 }
