3e40ddb0aca23861dd7e36b2993e8441b8584f70
fabriciorsf <fabriciorsf@baa973d8-b457-11dd-81ae-51688d75609f>
2009-02-12 12:00:34 +0000

    ScoreFinalDinamic

---
+++ trunk/QualityRanking/src/HelperScoreFinalDinamic.java
@@ -0,0 +1,120 @@
+import java.awt.Component;
+import java.util.Collection;
+import java.util.HashMap;
+
+import javax.swing.JOptionPane;
+
+import br.ufrj.cos.bean.ContextQualityDimensionWeight;
+import br.ufrj.cos.bean.DataSet;
+import br.ufrj.cos.bean.Document;
+import br.ufrj.cos.bean.QualityDimension;
+import br.ufrj.cos.db.HelperAcessDB;
+import br.ufrj.cos.db.HibernateDAO;
+import br.ufrj.cos.matlab.exception.MatLabException;
+import br.ufrj.cos.services.Service;
+
+/**
+ * 
+ */
+
+/**
+ * @author Fabricio
+ * 
+ */
+public class HelperScoreFinalDinamic {
+
+	/**
+	 * @param idDataSet
+	 * @param weightREP
+	 * @param weightCOM
+	 * @param weightTIM
+	 */
+	public static String generateScores(Long idDataSet, int weightREP,
+			int weightCOM, int weightTIM, Component component) {
+		HashMap<String, Integer> mapWeights = putWeights(weightREP, weightCOM,
+				weightTIM);
+
+		DataSet dataSet = (DataSet) HibernateDAO.getInstance().loadById(
+				DataSet.class, idDataSet);
+
+		if (dataSet != null) {
+			return reFuzzyDataSet(dataSet, mapWeights, component);
+		}
+		return "";
+	}
+
+	public static String reFuzzyDataSet(DataSet dataSet,
+			HashMap<String, Integer> mapWeights, Component component) {
+		String result = "";
+		Collection<ContextQualityDimensionWeight> listCQDWeights = HelperAcessDB
+				.loadContextQualityDimensionWeights(dataSet);
+
+		int qtdQualityDimensions = HelperAcessDB.loadQualityDimensions(dataSet)
+				.size();
+
+		double contextWeights[] = getWeights(listCQDWeights, mapWeights);
+
+		int o = JOptionPane.OK_OPTION;
+		Collection<Document> documents = HelperAcessDB.loadDocuments(dataSet);
+		for (Document document : documents) {
+
+			double[] qds = HelperAcessDB
+					.loadDocumentQualityDimensionScores(document);
+
+			Double documentScore = null;
+			while (documentScore == null) {
+				try {
+					documentScore = Service.fuzzy(qtdQualityDimensions, qds,
+							contextWeights);
+				} catch (MatLabException mle) {
+					System.err.println(mle.getMessage());
+					o = JOptionPane
+							.showConfirmDialog(
+									component,
+									"Possivelmente o servi�o do MatLab n�o est� iniciado ou houve erro no mesmo!\n"
+											+ "� necess�rio (re)iniciar o servi�o do MatLab antes de continuar!\n"
+											+ "Deseja continuar?",
+									"Erro no MatLab",
+									JOptionPane.WARNING_MESSAGE);
+					if (o != JOptionPane.OK_OPTION) {
+						break;
+					}
+				}
+			}
+
+			if (o != JOptionPane.OK_OPTION) {
+				break;
+			}
+
+			result = result
+					+ String.format("%f\n", documentScore.doubleValue());
+		}
+		System.out.println(result);
+		return result;
+	}
+
+	/**
+	 * @return
+	 */
+	private static HashMap<String, Integer> putWeights(int weightREP,
+			int weightCOM, int weightTIM) {
+		HashMap<String, Integer> mapWeights = new HashMap<String, Integer>();
+		mapWeights.put(QualityDimension.REP, new Integer(weightREP));
+		mapWeights.put(QualityDimension.COM, new Integer(weightCOM));
+		mapWeights.put(QualityDimension.TIM, new Integer(weightTIM));
+		return mapWeights;
+	}
+
+	private static double[] getWeights(
+			Collection<ContextQualityDimensionWeight> listCQDWeights,
+			HashMap<String, Integer> mapWeights) {
+		double[] weights = new double[listCQDWeights.size()];
+		int i = 0;
+		for (ContextQualityDimensionWeight contextQualityDimensionWeight : listCQDWeights) {
+			weights[i++] = mapWeights.get(
+					contextQualityDimensionWeight.getQualityDimension()
+							.getCodeStr()).doubleValue();
+		}
+		return weights;
+	}
+}
