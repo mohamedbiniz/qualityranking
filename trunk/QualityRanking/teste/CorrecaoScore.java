15d62c430fce04c11801b54f0c88752411919f33
fabriciorsf <fabriciorsf@baa973d8-b457-11dd-81ae-51688d75609f>
2009-03-04 19:47:09 +0000

    correção para os valores do metadado dataDeAtualizacao e do timeliness

--- trunk/QualityRanking/teste/CorrecaoScore.java
+++ trunk/QualityRanking/teste/CorrecaoScore.java
@@ -1,19 +1,42 @@
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 import br.ufrj.cos.bean.DataSet;
+import br.ufrj.cos.bean.Document;
+import br.ufrj.cos.bean.DocumentQualityDimension;
+import br.ufrj.cos.bean.Metadata;
+import br.ufrj.cos.bean.QualityDimension;
+import br.ufrj.cos.db.HelperAcessDB;
 import br.ufrj.cos.db.HibernateDAO;
+import br.ufrj.cos.enume.MetadataType;
 import br.ufrj.cos.services.Service;
+import br.ufrj.cos.services.process.MetadataExtract;
 
 public class CorrecaoScore {
+
+	private static Date now = new Date();
+
+	private static HibernateDAO dao = null;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		HibernateDAO.getInstance().openSession();
+		setDao(HibernateDAO.getInstance());
+		getDao().openSession();
 		DataSet dataSet = (DataSet) HibernateDAO.getInstance().loadById(
 				DataSet.class, new Long(578));
 		try {
-			Service.fuzzyDataSet(dataSet);
+			// Service.fuzzyDataSet(dataSet);
+			corrigirMetadataDate(dataSet);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -27,6 +50,112 @@ public class CorrecaoScore {
 		// l, i, l, i, l, i, l, i));
 		// }
 
+	}
+
+	private static void corrigirMetadataDate(DataSet dataSet) throws Exception {
+		DocumentQualityDimension documentQualityDimension = null;
+
+		List<Document> documents = HelperAcessDB.loadDocuments(dataSet);
+		Collection<QualityDimension> qualityDimensions = HelperAcessDB
+				.loadQualityDimensions(dataSet);
+
+		for (Document document : documents) {
+			System.gc();
+			MetadataExtract metadataExtract = new MetadataExtract(document
+					.getUrl());
+			Metadata metadata = HelperAcessDB.loadMetadata(document,
+					MetadataType.DATE);
+
+			metadata.setValue(metadataExtract.extract().get(MetadataType.DATE));
+
+			for (QualityDimension qualityDimension : qualityDimensions) {
+				documentQualityDimension = HelperAcessDB
+						.loadDocumentQualityDimension(document,
+								qualityDimension);
+
+				double score = 0;
+				String code = qualityDimension.getCodeStr();
+				if (code.equals(QualityDimension.TIM)) {
+					score = getTimeliness(document, metadata);
+					documentQualityDimension.setScore(new BigDecimal(score));
+					getDao().update(documentQualityDimension);
+				}
+
+			}
+
+		}
+	}
+
+	private static double getTimeliness(Document document, Metadata metadata) {
+		double score = 0;
+		byte[] metadataValue = metadata.getValue();
+		if (metadataValue != null) {
+			DateFormat dateFormat = new SimpleDateFormat(
+					MetadataExtract.DATE_FORMAT);
+			Date lastModified = null;
+			try {
+				lastModified = dateFormat.parse(new String(metadataValue));
+			} catch (ParseException e) {
+				System.err.println(String.format(
+						"Este documento (%s) não possui o metadado, "
+								+ "ou o mesmo não está no formato correto, "
+								+ "para calcular o timeliness", document
+								.toString()));
+			}
+			if (lastModified != null) {
+				double diffDates = calcDiffDays(getNow(), lastModified);
+				double quo = (diffDates + 1);
+				score = 1 / (quo > 1 ? quo : 1);
+			}
+		}
+		return score;
+	}
+
+	private static double calcDiffDays(Date dateEnd, Date dateInit) {
+		// Creates two calendars instances
+		Calendar calInit = new GregorianCalendar();
+		Calendar calEnd = new GregorianCalendar();
+
+		// Set the date for both of the calendar instance
+		calInit.setTime(dateInit);
+		calEnd.setTime(dateEnd);
+
+		// Get the represented date in milliseconds
+		long milisInit = calInit.getTimeInMillis();
+		long milisEnd = calEnd.getTimeInMillis();
+
+		// Calculate difference in milliseconds
+		double diff = milisEnd - milisInit;
+
+		//
+		// // Calculate difference in seconds
+		// double diffSeconds = diff / 1000;
+		//
+		// // Calculate difference in minutes
+		// double diffMinutes = diff / (60 * 1000);
+		//
+		// // Calculate difference in hours
+		// double diffHours = diff / (60 * 60 * 1000);
+
+		// Calculate difference in days
+		double diffDays = diff / (24 * 60 * 60 * 1000);
+		return diffDays;
+	}
+
+	public static Date getNow() {
+		return now;
+	}
+
+	public static void setNow(Date now) {
+		CorrecaoScore.now = now;
+	}
+
+	public static HibernateDAO getDao() {
+		return dao;
+	}
+
+	public static void setDao(HibernateDAO dao) {
+		CorrecaoScore.dao = dao;
 	}
 
 }
